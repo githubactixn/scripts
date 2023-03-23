ed64f0dedb96e30c6d2f21584fbcb7edd6d3b3e1
z3zens <78209717+z3zens@users.noreply.github.com>
2023-03-23 19:56:15 +0700

    kernel.sh: Update name kernel

--- kernel.sh
+++ kernel.sh
@@ -54,7 +54,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="perf"
+ZIPNAME="LA.UM_4.19"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
